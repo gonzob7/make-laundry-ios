86774761f7e17440d19274fc32266692d8d09f40
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-01-09 14:41:51 -0800

    updated keyboard to hide when touched anywhere outside

--- MakeLaundry/LoginViewController.swift
+++ MakeLaundry/LoginViewController.swift
@@ -21,6 +21,9 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
 //         Do any additional setup after loading the view.
 
+        let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tapGesture)
+        
         
     }
 
